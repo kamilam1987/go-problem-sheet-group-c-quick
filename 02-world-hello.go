cf0c57d4c516599d85726120c96666a66fa118cd
KAMILA MICHEL - Student <G00340498@gmit.ie>
2017-09-19 15:42:50 +0100

    second commit

---
+++ 02-world-hello.go
@@ -0,0 +1,8 @@
+// Peano integers are represented by a linked
+// list whose nodes contain no data
+// (the nodes are the data).
+// http://en.wikipedia.org/wiki/Peano_axioms
+
+// This program demonstrates that Go's automatic
+// stack management can handle heavily recursive
+// computations.
